32132dad640f08df21b49f0b3e22653f245f497b
Stuart Thompson <stuart.thompson@nytimes.com>
2018-09-04 21:43:43 -0400

    Access

--- src/mongodbConnModule.js
+++ src/mongodbConnModule.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 module.exports.connect = function() {
 	//mongoose.connect('mongodb://localhost:27017/scheduler');
-	mongoose.connect('mongodb://SuperAdmin:Kihshlb1@ds145072.mlab.com:45072/cityscheduler_test');
+	mongoose.connect('mongodb://SuperAdmin:KetzaWicket1@ds145072.mlab.com:45072/cityscheduler_test');
 	var db = mongoose.connection;
 	db.on("error", console.error.bind(console, "connection error"));
 	db.once("open", function(callback){
